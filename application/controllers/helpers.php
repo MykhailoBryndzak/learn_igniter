ced233536205610e3feceee8253fa893760db82e
mike <mykhailo.bryndzak@gmail.com>
2016-01-19 16:23:50 +0200

    helper string

--- application/controllers/helpers.php
+++ application/controllers/helpers.php
@@ -8,18 +8,7 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
          $this->load->helper('text');
          $this->load->helper('string');
 
-//         $vals = array(
-//             'word'	        => 'Random word',
-//             'img_path  '  	=> './img/capcha',
-//             'img_url'	    => base_url().'/img/capcha',
-//             'font_path'	=> './path/to/fonts/texb.ttf',
-//             'img_width'	=> '150',
-//             'img_height'   => 30,
-//             'expiration'   => 10
-//         );
-//
-//         $cap = create_captcha($vals);
-//         echo $cap['image'];
+
 
          $this->load->view('helpers_view');
 

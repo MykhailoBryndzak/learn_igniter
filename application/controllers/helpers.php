664201dde22e4ca4692706c0c06f274eaaa03fc2
mike <mykhailo.bryndzak@gmail.com>
2016-01-19 16:37:37 +0200

    captcha

--- application/controllers/helpers.php
+++ application/controllers/helpers.php
@@ -7,8 +7,22 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
          $this->load->helper('html');
          $this->load->helper('text');
          $this->load->helper('string');
+         $this->load->helper('captcha');
 
+         $random_string = random_string('numeric', 5);
 
+         $vals = array(
+             'word'	        => $random_string,
+             'img_path  '  	=> './img/captcha/',
+             'img_url'	    => base_url().'/img/captcha/',
+             'font_path'	=> './system/fonts/texb.ttf',
+             'img_width'	=> 150,
+             'img_height'   => 30,
+             'expiration'   => 10
+         );
+
+         $cap = create_captcha($vals);
+         echo $cap['image'];
 
          $this->load->view('helpers_view');
 
--- application/views/helpers_view.php
+++ application/views/helpers_view.php
@@ -4,16 +4,16 @@
 
 <?php
 
-echo (random_string('alnum', 8));
-    $string = " mike,,pike,,bryndza,,";
-echo (reduce_multiples($string,","));
+//echo (random_string('alnum', 8));
+//    $string = " mike,,pike,,bryndza,,";
+//echo (reduce_multiples($string,","));
+//
+//$string = "\n";
+//echo repeater($string, 30);
+//
+//$string = "http://example.com//index.php";
+//echo reduce_double_slashes($string);
 
-$string = "\n";
-echo repeater($string, 30);
-
-$string = "http://example.com//index.php";
-echo reduce_double_slashes($string);
-// сделает "http://example.com/index.php"
 
 ?>
 
